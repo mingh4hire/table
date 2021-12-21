6a2f67bd844967cc0e84e0ae48734f74797262bd
Bob <mingh.dev@gmail.com>
2021-12-21 15:08:14 -0600

    more code

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable,   } from 'rxjs';
+import { startWith , map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-table',
@@ -10,18 +13,45 @@ export class TableComponent implements OnInit {
 data: Array<any>;
 @Input()
 headers: Array<{key:string, val:string}>;
-searchTerm: string = null;
+searchTerm: FormControl = new FormControl();
 page = 1;
 pageSize = 10;
 total:number;
+results$ ;
+
 
   constructor() { 
+  }
+
+  filter(term:string):Array<any>{
+    if (!term){
+      return this.data;
+    }
+    return this.data.filter(row=>{
+      for (const header of this.headers){
+        console.log(header.key, ' is hte key');
+        console.log(row[header.key], ' is the key value');
+        if (row[header.key]?.toString()?.indexOf(term) >= 0){
+            return true;
+        }
+      }
+    })
   }
 
   ngOnInit(): void {
     console.log(2);
     console.log(1);
     this.total = this.data?.length;
-    
+    this.results$ = this.searchTerm.valueChanges.pipe(startWith(''),
+      map(term=>{
+      return this.filter(term);
+    }));
+    console.log('ki s');
+   }
+  get term(){
+    return this.searchTerm.value;
   }
+ 
+
 }
+
